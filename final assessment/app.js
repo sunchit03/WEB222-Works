ee03e62866d23a8698a90daa195acaaf5de7b6e0
Sunchit Singh <sunchit-singh@myseneca.ca>
2022-12-06 13:25:04 -0500

    final assessment

---
+++ final assessment/app.js
@@ -0,0 +1,38 @@
+var payDiv = document.getElementById("pay-div");
+
+
+payDiv.innerHTML = "";
+
+let payLabel = document.createElement("label");
+payLabel.id = "pay-lbl";
+payLabel.for = "pay-rate";
+payLabel.classList.add("form-label");
+payLabel.innerText = "Hourly Rate";
+
+let payRate = document.createElement("input");
+payRate.id = "pay-rate";
+payRate.classList.add("form-control");
+payRate.type = "text";
+payRate.name = "pay-rate";
+payRate.tabIndex = "8";
+payRate.pattern = "^\\d*[.]?\\d+$";
+payRate.required = true;
+
+
+
+
+ function checkRadio() {
+
+    let hiring = document.getElementById("hiring");
+    payDiv.innerHTML = "";
+
+    if (hiring.checked === true) {
+        
+        /* <label id="pay-lbl" for="pay-rate" class="form-label">Hourly Rate</label>
+            <input id="pay-rate" class="form-control" type="text" name="pay-rate" tabindex="8" pattern="^\d*[.]?\d+$" required>
+        */
+        
+        payDiv.appendChild(payLabel);
+        payDiv.appendChild(payRate);
+    }
+ };
