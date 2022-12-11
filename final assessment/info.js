7065c046e2a9f4b64493ca7eb7d56323762bf3c3
Sunchit Singh <sunchit-singh@myseneca.ca>
2022-12-11 00:03:01 -0500

    cover pic added; media query added for cover pic; pay-rate type changed to num reflection added

--- final assessment/info.js
+++ final assessment/info.js
@@ -1,5 +1,19 @@
-var payDiv = document.getElementById("pay-div");
+var minWage = {
+    "Toronto": 15.50,
+    "Vancouver": 15.65,
+    "Montreal": 14.25,
+    "Niagara Falls": 15.50,
+    "Victoria": 15.65,
+    "Halifax": 13.60,
+    "Quebec City": 14.25,
+    "Calgary": 15.00,
+    "Ottawa": 15.50,
+    "Edmonton": 15.00
+}
 
+
+var payDiv = document.getElementById("pay-div");
+var city = document.getElementById("city");
 
 payDiv.innerHTML = "";
 
@@ -19,10 +33,30 @@ payLabel.innerHTML += "Hourly Rate";
 let payRate = document.createElement("input");
 payRate.id = "pay-rate";
 payRate.classList.add("form-control");
-payRate.type = "text";
+payRate.type = "number";
 payRate.name = "pay-rate";
 payRate.tabIndex = "8";
-payRate.pattern = "^\\d*[.]?\\d+$";
+//payRate.pattern = "^\\d*[.]?\\d+$";
+payRate.step = "0.01";
+
+
+
+window.onload = function() {
+    city.addEventListener("change", () => {
+        if (city && city.value) {
+            var flag = 0;
+            payRate.min = "15.50";
+            for (const key in minWage) {
+                if (flag == 0 && city.value === key) {
+                    payRate.min = `${minWage[key]}`;
+                    flag = 1;
+                }
+            }
+        }
+    });
+}
+
+
 payRate.required = true;
 
 
@@ -40,6 +74,16 @@ payRate.required = true;
         */
         
         payDiv.appendChild(payLabel);
+        // if (city && city.value) {
+        //     var flag = 0;
+            
+        //     for (const key in minWage) {
+        //         if (flag == 0 && city.value === key) {
+        //             payRate.min = `${minWage[key]}`;
+        //             flag = 1;
+        //         }
+        //     }
+        // }
         payDiv.appendChild(payRate);
     }
  };
